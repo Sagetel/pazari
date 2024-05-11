c9d3e640ac7cd63a685d4c07d5b493b45eed6748
Sagetel <102986847+Sagetel@users.noreply.github.com>
2024-05-11 15:20:59 +0300

    final fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,30 +19,28 @@ export default function RootLayout({
         <Head>
           <link rel="preload" href="https://mc.yandex.ru/metrika/tag.js" as="script" />
         </Head>
-
         <script
           dangerouslySetInnerHTML={{
             __html: `
-        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
-
-        ym(97251244,"init",{
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-        });
-      `,
+            (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
+            ym(97251244,"init",{
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `,
           }}
         />
 
-        <div><img src="https://mc.yandex.ru/watch/97251244" style={{ position: `absolute`, left: `-9999px` }} alt="" /></div>
+        <img src="https://mc.yandex.ru/watch/97251244" style={{ position: 'absolute', left: '-9999px' }} alt="" />
         <Header />
         {children}
       </body>
-    </html>
+    </html >
   );
 }
