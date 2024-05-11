ddf7fde750d8561bb7f140b8491ff271edd24273
Sagetel <102986847+Sagetel@users.noreply.github.com>
2024-05-11 14:48:57 +0300

    metrika fix 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,27 +18,28 @@ export default function RootLayout({
       <body>
         <Head>
           <link rel="preload" href="https://mc.yandex.ru/metrika/tag.js" as="script" />
-
-          <script dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
-
-              ym(97251244,"init",{
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-              });
-            `,
-          }} />
         </Head>
-        <noscript>
-          <div><img src="https://mc.yandex.ru/watch/97251244" style={{ position: `absolute`, left: `-9999px;` }} alt="" /></div>
-        </noscript>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
+
+        ym(97251244,"init",{
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+        });
+      `,
+          }}
+        />
+
+        <img src="https://mc.yandex.ru/watch/97251244" style={{ position: 'absolute', left: '-9999px' }} alt="" />
         <Header />
         {children}
       </body>
